46e6d67871fba4ac66329ac061d9ed5b9b7991bd
sobbingtofu <kukubasak@naver.com>
2024-05-29 05:12:06 +0900

    initialize: redux 도입 시작 및 기본 구조 잡기

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./redux/config/configStore.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
